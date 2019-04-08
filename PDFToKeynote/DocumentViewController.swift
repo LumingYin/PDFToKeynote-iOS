301b5c3f491d0e578d661bcbce9979fd682c15b3
Luming Yin <vista980622@gmail.com>
2019-04-07 21:09:00 -0400

    fix

--- PDFToKeynote/DocumentViewController.swift
+++ PDFToKeynote/DocumentViewController.swift
@@ -54,7 +54,7 @@ class DocumentViewController: UIViewController {
         var templateBeginning = stringForTextFileName("template_beginning")
         var templateContent = stringForTextFileName("template_content")
         var templateEnding = stringForTextFileName("template_ending")
-        print(templateEnding)
+//        print(templateEnding)
 
         var actualContent = ""
         templateBeginning = templateBeginning.replacingOccurrences(of: "xxxSWIDTHxxx", with: "1024")
@@ -70,12 +70,63 @@ class DocumentViewController: UIViewController {
         templateContent = templateContent.replacingOccurrences(of: "xxxPOSXxxx", with: "0")
         templateContent = templateContent.replacingOccurrences(of: "xxxPOSYxxx", with: "0")
 
-        for count in 0..<count {
-            var pageContent = templateContent.replacingOccurrences(of: "yyySNUMyyy", with: "\(count)")
-            let pageString = String(format: "pg_%04d.pdf", (count + 1))
-            pageContent = pageContent.replacingOccurrences(of: "xxxFILENAMExxx", with: pageString)
-            actualContent = "\(actualContent)\(pageContent)"
+        var sfaIDCachedDict: [String : Int] = [:]
+        var sfaIDRefCachedDict: [String : Int] = [:]
+
+        do {
+            for count in 0..<count {
+                var pageContent = templateContent
+                var eachLine = pageContent.components(separatedBy: .newlines)
+                for i in 0..<eachLine.count {
+                    let lineContent = eachLine[i]
+                    let pattern = #"(sfa:ID="((\D+?)-\d+)")"#
+                    let regex = try NSRegularExpression(pattern: pattern, options: [])
+                    if let match = regex.firstMatch(in: lineContent, range: NSRange(lineContent.startIndex..., in: lineContent)) {
+                        let part1 = String(lineContent[Range(match.range(at: 1), in: lineContent)!])
+                        let part2 = String(lineContent[Range(match.range(at: 2), in: lineContent)!])
+                        print("Part 1: \(part1), part 2:\(part2)")
+//                        print(String(part1 + part2))
+                        if sfaIDCachedDict[part2] == nil {
+                            sfaIDCachedDict[part2] = 1000
+                        } else {
+                            sfaIDCachedDict[part2] = sfaIDCachedDict[part2]! + 1
+                        }
+                        let reconstructedIndex = "sfa:ID=\"\(part2)-\(sfaIDCachedDict[part2]!)\""
+                        let replacementRange = NSMakeRange(0, lineContent.count)
+                        let modString = regex.stringByReplacingMatches(in: lineContent, options: [], range: replacementRange, withTemplate: reconstructedIndex) // reconstructedIndex is not a template
+                        eachLine[i] = modString
+                    }
+                }
+
+//                for i in 0..<eachLine.count {
+//                    let lineContent = eachLine[i]
+//                    let pattern = #"sfa:IDREF="((\D+?)-\d+)""#
+//                    let regex = try NSRegularExpression(pattern: pattern, options: [])
+//                    if let match = regex.firstMatch(in: lineContent, range: NSRange(lineContent.startIndex..., in: lineContent)) {
+//                        let part1 = String(lineContent[Range(match.range(at: 1), in: lineContent)!])
+//                        let part2 = String(lineContent[Range(match.range(at: 2), in: lineContent)!])
+//                        print("Part 1: \(part1), part 2:\(part2)")
+//                        //                        print(String(part1 + part2))
+//                        if sfaIDRefCachedDict[part2] == nil {
+//                            sfaIDRefCachedDict[part2] = 1000
+//                        } else {
+//                            sfaIDRefCachedDict[part2] = sfaIDRefCachedDict[part2]! + 1
+//                        }
+//                        let reconstructedIndex = "sfa:IDREF=\"\(part2)-\(sfaIDRefCachedDict[part2]!)\""
+//                        let replacementRange = NSMakeRange(0, lineContent.count)
+//                        let modString = regex.stringByReplacingMatches(in: lineContent, options: [], range: replacementRange, withTemplate: reconstructedIndex) // reconstructedIndex is not a template
+//                        eachLine[i] = modString
+//                    }
+//                }
+
+                let pageString = String(format: "pg_%04d.pdf", (count + 1))
+                pageContent = pageContent.replacingOccurrences(of: "xxxFILENAMExxx", with: pageString)
+                actualContent = "\(actualContent)\(eachLine.joined(separator: "\n"))"
+            }
+        } catch {
+            print(error)
         }
+
 
         var finalText = "\(templateBeginning)\(actualContent)\(templateEnding)"
 //        print(finalText)
